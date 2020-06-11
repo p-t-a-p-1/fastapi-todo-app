9d3581280575dd81b2348718334cd59c82bbfcda
p-t-a-p-1 <kb240225+1@gmail.com>
2020-06-11 22:01:36 +0900

    [fix] jinja2を使えるように修正（dockerfile修正）

--- app/main.py
+++ app/main.py
@@ -7,9 +7,7 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-
 templates = Jinja2Templates(directory="templates")
-
 
 @app.get("/items/{id}")
 async def read_item(request: Request, id: str):
