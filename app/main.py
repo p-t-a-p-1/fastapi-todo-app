b641dafa95a588587355ec232770680aacce1944
p-t-a-p-1 <kb240225+1@gmail.com>
2020-06-11 22:53:34 +0900

    [update] CSSフレームワークにBulma採用しベースコンテンツ作成

--- app/main.py
+++ app/main.py
@@ -3,11 +3,19 @@ from starlette.requests import Request
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
-app = FastAPI()
+app = FastAPI(
+    title='FastAPIで作成するTODOアプリ',
+    description='シンプルなTODOアプリです',
+    version='0.9 beta'
+)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+def index(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
 
 @app.get("/items/{id}")
 async def read_item(request: Request, id: str):
